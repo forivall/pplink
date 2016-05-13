a245956e8e5d709e466260b69f0a76566d1ca880
Jordan Klassen <jordan@klassen.me.uk>
2016-05-13 00:14:15 -0700

    feat: add --save{,-dev,-optional,-bundle} option

--- bin/pplink.js
+++ bin/pplink.js
@@ -6,44 +6,62 @@ var path = require('path');
 var mkdirp = require('mkdirp');
 
 var createLinkedDep = require('../index');
+var saveDep = createLinkedDep.saveDep;
 
 function program(argv) {
   var args = require('minimist')(argv, {
     string: ['name'],
-    boolean: ['relative', 'help'],
+    boolean: ['relative', 'help', 'save', 'save-dev', 'save-optional', 'save-bundle'],
     alias: {
-      relative: ['r']
+      relative: ['r'],
+      save: ['s']
     }
   });
 
   if (args._.length < 1 || args.help) {
     return fs.createReadStream(path.join(__dirname, '/usage.txt'))
     .pipe(process.stdout)
-    .on('close', function () { process.exit(Number(!args.help)); });
+    .on('close', function () {
+      process.exit(Number(!args.help));
+    });
   }
 
   var src = args._[0];
-  var dest = args._[1] || '.';
+  var destArg = args._[1] || '.';
 
-  fs.stat(path.join(dest, 'package.json'), function (err, stats) {
+  fs.stat(path.join(destArg, 'package.json'), function (err, stats) {
+    var dest = destArg;
+
     if (err) {
       if (err.code === 'ENOENT') {
         // if no package.json exists in the cwd, link directly
-        return createLinkedDep(src, dest, args, exit);
+        return createLinkedDep(src, dest, args, doSaveDep);
       }
       return exit(err);
     }
 
     if (stats.isFile()) {
-      dest = path.join(dest, 'node_modules');
+      dest = path.join(destArg, 'node_modules');
       return mkdirp(dest, function (err) {
         if (err) return exit(err);
-        return createLinkedDep(src, dest, args, exit);
+        return createLinkedDep(src, dest, args, doSaveDep);
       });
     }
 
-    return createLinkedDep(src, dest, args, exit);
+    return createLinkedDep(src, dest, args, doSaveDep);
   });
+
+  function doSaveDep(err, pkg) {
+    if (err) return exit(err);
+    if (!args.save && !args['save-dev'] && !args['save-bundle'] && !args['save-optional']) {
+      return exit();
+    }
+    var saveAs =
+      args['save-dev'] ? 'dev' :
+      args['save-bundle'] ? 'bundle' :
+      args['save-optional'] ? 'optional' : null;
+    saveDep(pkg, destArg, {saveAs: saveAs}, exit);
+  }
 
   function exit(err) {
     if (err) console.error(err.stack || err);
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var fs = require('fs');
 var path = require('path');
 
 var mkdirp = require('mkdirp');
+var updatePackage = require('update-package-json');
+var sortKeys = require('sort-keys');
 
 module.exports = createLinkedDep;
 function createLinkedDep(src, dest, options, callback) {
@@ -15,6 +17,8 @@ function createLinkedDep(src, dest, options, callback) {
   });
 }
 
+// src is a path to the package that we're linking to
+// dest is a directory path
 module.exports.createLinkedDepFiles = createLinkedDepFiles;
 function createLinkedDepFiles(src, dest, options, callback) {
   if (arguments.length === 3 && typeof options === 'function') {
@@ -34,13 +38,43 @@ function createLinkedDepFiles(src, dest, options, callback) {
   fs.writeFile(path.join(dest, 'package.json'), JSON.stringify({
     name: options.name || pkg.name,
     version: pkg.version
-  }, null, '  '), function (err) {
+  }, null, '  ') + '\n', function (err) {
     if (err) return callback(err);
 
     fs.writeFile(
       path.join(dest, 'index.js'),
       'module.exports = require(' + JSON.stringify(src) + ');',
-      callback
+      function (err) {
+        if (err) return callback(err);
+        callback(null, pkg);
+      }
     );
+  });
+}
+
+module.exports.saveDep = saveDep;
+function saveDep(pkg, dest, options, callback) {
+  if (arguments.length === 3 && typeof options === 'function') {
+    callback = options;
+    options = {};
+  }
+
+  var destKey =
+    options.saveAs === 'dev' ? 'devDependencies' :
+    options.saveAs === 'optional' ? 'optionalDependencies' :
+    options.saveAs === 'bundle' ? 'bundleDependencies' :
+    'dependencies';
+
+  fs.stat(dest, function (err, stats) {
+    if (err) return callback(err);
+    if (stats.isDirectory()) {
+      dest = path.join(dest, 'package.json');
+    }
+    updatePackage(dest, function (destpkg) {
+      var deps = destpkg[destKey] || {};
+      deps[options.name || pkg.name] = '^' + (options.version || pkg.version);
+      destpkg[destKey] = sortKeys(deps);
+    }, callback);
+    // NOTE: update-package-json doesn't report an error when it can't read
   });
 }
